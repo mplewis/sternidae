bf8d2a7716e452dc125c17a3a15bb413dace5107
Matthew Lewis <matt@mplewis.com>
2016-04-28 18:02:56 -0500

    sketch: progress calculating distance and time

--- src/src.ino
+++ src/src.ino
@@ -1,40 +1,61 @@
 #include <math.h>
 
 typedef struct Location {
-  char name[16];
   double lat;
   double lng;
 } Location;
 
-unsigned long updateAt = 0;
+typedef struct Destination {
+  char name[16];
+  Location loc;
+} Destination;
 
+// Updated by onNewGps
+// Location
 Location currentLoc = {0};
-Location lastLoc = {0};
-double currentMph = 0;
-double currentHeading = 0;
+// Time in seconds
+unsigned long currentTime = 0;
+unsigned long lastTime = 0;
+double deltaTime = 0;
+// Distance in degrees
+double currentDist = 0;
+double lastDist = 0;
+double deltaDist = 0;
+// ETA in seconds. If negative, we're traveling away from currentDest
+double etaTime = 0;
 
-Location locations[] = {
-  {"Home", 45.022884, -93.244983},
-  {"Work", 44.989136, -93.250454},
+Destination dests[] = {
+  {"Test", {0.0, 10.0}},
+  {"Home", {45.022884, -93.244983}},
+  {"Work", {44.989136, -93.250454}},
 };
+
+Location currentDest = dests[0].loc;
 
 double distance(Location from, Location to) {
   return sqrt(pow(from.lat - to.lat, 2) + pow(from.lng - to.lng, 2));
 }
 
-void onNewGps(double lat, double lng, double mph, double heading) {
+void onNewGps(double lat, double lng) {
   currentLoc.lat = lat;
   currentLoc.lng = lng;
-  currentMph = mph;
-  Serial.println(currentLoc.lat, 6);
-  Serial.println(currentLoc.lng, 6);
-  Serial.println(distance(lastLoc, currentLoc));
-  lastLoc = currentLoc;
+
+  lastTime = currentTime;
+  currentTime = millis();
+  deltaTime = currentTime - lastTime;
+
+  lastDist = currentDist;
+  currentDist = distance(currentLoc, currentDest);
+  deltaDist = currentDist - lastDist;
+
+  etaTime = -deltaTime * currentDist / deltaDist;
 }
 
 void setup() {
   Serial.begin(9600);
 }
+
+unsigned long updateAt = 0;
 
 void loop() {
   if (millis() >= updateAt) {
@@ -42,9 +63,14 @@ void loop() {
     int ticks = (millis() / 1000) % 24;
     onNewGps(
       (double) cos(ticks * (2 * 3.14159 / 24)),
-      (double) sin(ticks * (2 * 3.14159 / 24)),
-      (double) random(0, 8000) / 100,
-      (double) random(0, 360)
+      (double) sin(ticks * (2 * 3.14159 / 24))
     );
+    Serial.print(currentLoc.lat, 6);
+    Serial.print(", ");
+    Serial.println(currentLoc.lng, 6);
+    Serial.println(currentDist);
+    Serial.println(deltaDist);
+    Serial.println(etaTime);
+    Serial.println();
   }
 }
