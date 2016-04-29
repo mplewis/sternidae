e6df6445b32a8bfc802c592fd53135048e9beda7
Matthew Lewis <matt@mplewis.com>
2016-04-28 21:31:31 -0500

    sketch: add averaging, layout, comments

--- src/src.ino
+++ src/src.ino
@@ -1,14 +1,23 @@
 #include <math.h>
 
+// A physical location
 typedef struct Location {
   double lat;
   double lng;
 } Location;
 
+// A name attached to a physical location
 typedef struct Destination {
   char name[16];
   Location loc;
 } Destination;
+
+// Waypoints to be used for navigation
+Destination dests[] = {
+  {"Test", {0.0, 10.0}},
+  {"Home", {45.022884, -93.244983}},
+  {"Work", {44.989136, -93.250454}},
+};
 
 // Updated by on_new_gps
 // Location
@@ -24,11 +33,10 @@ double delta_dist = 0;
 // ETA in milliseconds. If negative, we're traveling away from current_dest
 double eta_time = 0;
 
-Destination dests[] = {
-  {"Test", {0.0, 10.0}},
-  {"Home", {45.022884, -93.244983}},
-  {"Work", {44.989136, -93.250454}},
-};
+// For averaging ETA
+const int avg_count = 10;
+double avg_buf[avg_count] = {0};
+int avg_buf_pos = 0;
 
 // Change current_dest to change the distance and eta
 Location current_dest = dests[0].loc;
@@ -38,6 +46,22 @@ double distance(Location from, Location to) {
   return sqrt(pow(from.lat - to.lat, 2) + pow(from.lng - to.lng, 2));
 }
 
+// Stores a value for the sliding window average
+void put_avg(double val) {
+  avg_buf[avg_buf_pos] = val;
+  avg_buf_pos = (avg_buf_pos + 1) % avg_count;
+}
+
+// Gets the average of all stored values
+double get_avg() {
+  double avg = 0;
+  for (int i = 0; i < avg_count; i++) {
+    avg += avg_buf[i] / avg_count;
+  }
+  return avg;
+}
+
+// Called when the GPS has a new location
 void on_new_gps(double lat, double lng) {
   current_loc.lat = lat;
   current_loc.lng = lng;
@@ -51,14 +75,18 @@ void on_new_gps(double lat, double lng) {
   delta_dist = current_dist - last_dist;
 
   eta_time = -delta_time * current_dist / delta_dist;
+  put_avg(eta_time / 1000);
 }
 
+// Used for simulating GPS data
+unsigned long update_at = 0;
+
+// Start serial
 void setup() {
   Serial.begin(9600);
 }
 
-unsigned long update_at = 0;
-
+// Fake GPS data and print calculated values
 void loop() {
   if (millis() >= update_at) {
     update_at = millis() + 250;
@@ -72,7 +100,7 @@ void loop() {
     Serial.println(current_loc.lng, 6);
     Serial.println(current_dist);
     Serial.println(delta_dist);
-    Serial.println(eta_time / 1000);
+    Serial.println(get_avg());
     Serial.println();
   }
 }
